e1ffa46a70830adc371502add3739d77b8a5aa01
Oshan kodagoda <oshankodagoda20067@gmail.com>
2025-08-02 23:04:04 +0530

    rename keylistner to shelldaemon

--- shelldaemon.go
+++ shelldaemon.go
@@ -6,8 +6,7 @@ import (
 	"os"
 )
 
-
-func Shelldeamon(){
+func Keylistner(){
 logfile,err:= os.OpenFile("example.txt",os.O_WRONLY| os.O_APPEND |os.O_CREATE,0644 )
 if err!=nil{
 	fmt.Println("something is wrong",err)
@@ -17,6 +16,3 @@ defer logfile.Close()
 daemonlogger:=log.New(logfile,"LOG",log.Ldate|log.Ltime)
 daemonlogger.Println("Keylistner started")
 } 
-
-
-
