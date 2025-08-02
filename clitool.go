91791bb6216f380d71275f0bb87690ce2a0081e2
Oshan Kodagoda <oshankodagoda200667@gmail.com>
2025-08-02 23:54:43 +0530

    test

--- clitool.go
+++ clitool.go
@@ -6,46 +6,6 @@ import (
 	"os"
 	"os/user"
 )
-func Talk(){
-fmt.Println("helo appachchie")
-scanner:=bufio.NewReader(os.Stdin)
-fmt.Println("enter something yarr")
-userinput,err:=scanner.ReadString('\n')
-
-if err!=nil{fmt.Println("kelaa una")
-return
-}
-fmt.Println(userinput)
-
-}
-func ReadFile(){
-fmt.Println("this is a readfile method") 
-data,err:=os.ReadFile("textfile.txt")
-if err!=nil{
-fmt.Println("hukao yako")
-return
-
-}
-fmt.Println(string(data))
-}
-func ReadBigFile(){
-fmt.Println("Read big file now")
-data,err:=os.Open("README.md")
-if err!=nil{
-	fmt.Println("holyshit")
-return
-}
-defer data.Close()
-scanner:=bufio.NewScanner(data)
-for scanner.Scan(){
-fmt.Println(scanner.Text())
-}
-// Check for errors during scanning
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading file:", err)
-	}
-}
-
 
 func ReadHistory(){
 	fmt.Println("reading bash history rn.....")
