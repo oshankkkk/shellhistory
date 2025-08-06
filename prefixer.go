8a9d6b05c248ad8a8b889beb3e010a5fda8a53b5
Oshan Kodagoda <oshankodagoda200667@gmail.com>
2025-08-07 05:08:28 +0530

    add prefix trie to store prefixes

--- prefixer.go
+++ prefixer.go
@@ -1,25 +1,65 @@
 package main
 
 import "fmt"
+
+
+type Node struct{
+children map[string]*Node
+isEndLetter bool
+
+}
+
+func Nodeinit()*Node{
+return &Node{children: make(map[string]*Node),isEndLetter: false}
+}
+
 type Tree struct{
-
-
-	Content string
-	Children []*Tree
-}
-func Newtree(value string) *Tree{
-return &Tree{Content: value} 
+Root *Node
 }
 
-// why cant we just go &Tree{Content: value}?
-func(t *Tree) InsertNode(node string){
-	child :=Newtree(node)
-	t.Children=append(t.Children, child)
+func TreeInit()*Tree{
+return &Tree{
+Root: Nodeinit(),}
+}
+func(t *Tree)InsertLetter(word string){
+	currentnode:=t.Root
+	for _,value:=range word{
+		letter:=string(value)
+		if _,exits:= currentnode.children[letter]; exits{
+			currentnode=currentnode.children[letter]	
+		}else{
+			newnode:=Nodeinit()
+			currentnode.children[letter]=newnode
+			currentnode=currentnode.children[letter]
+
+		}
+		
+	} 
+currentnode.isEndLetter=true
+		fmt.Println("hari oya inserted ")
 }
 
-func(t *Tree) ShowTree(){
-	fmt.Println("Gonna print the tree now")
-	for _,value:=range t.Children{
-	fmt.Println(*&value.Content ," node")
+
+func helperShowtree(key string, currentnode *Node) {
+	fmt.Println(key)
+	if len(currentnode.children) > 0 {
+		for key, node := range currentnode.children {
+			helperShowtree(key, node)
+		}
 	}
 }
+
+func(t *Tree) Showtree(){
+	currentnode:=t.Root
+	for key,node:=range currentnode.children{
+
+helperShowtree(key,node)
+	}
+}
+
+
+
+
+
+
+
